6cc7c56b4f9dbf65a2743ee61d39380207338e11
EclipseReign <redenizzko@gmail.com>
2025-11-01 11:10:55 +0500

    same

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,4 +1,4 @@
-"import i18n from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
@@ -53,4 +53,3 @@ i18n
   });
 
 export default i18n;
-"
